4830f1b135bf391af8226b1a7c7035e635b98aed
Biowulf513 <cherepanov92@gmail.com>
2017-03-01 17:20:15 +0500

    add paginator

--- myfly/views.py
+++ myfly/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .models import Route
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def all(request):
-	routes = Route.objects.all()
-	return render(request, 'myfly/table.html', {'routes': routes})
+    routes_list = Route.objects.all()
+    paginator = Paginator(routes_list, 10)
+
+    page = request.GET.get('page')
+
+    try:
+        routes = paginator.page(page)
+    except PageNotAnInteger:
+        routes = paginator.page(1)
+    except EmptyPage:
+        routes = paginator.page(paginator.num_pages)
+
+    return render(request, 'myfly/table.html', {'routes': routes})
