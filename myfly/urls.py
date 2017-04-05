d595fb857e48fd6cb1b255b709e73e3e92db76d5
Biowulf513 <cherepanov92@gmail.com>
2017-04-05 13:57:44 +0500

    добавил app_name в URLs

--- myfly/urls.py
+++ myfly/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 from myfly.views import table
 
+app_name = 'myfly'
 urlpatterns = [
 	url(r'^$', views.table, name='table'),
 	url(r'^flight/(?P<route_id>[0-9]+)/$', views.flight, name='flight'),
